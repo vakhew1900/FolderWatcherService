159b93627d62402c2871e0b47f0a8ce99ac6076c
T0mmy <antchupinin@gmail.com>
2023-04-21 22:19:30 +0300

    add logger for folderWatcherService

--- FolderWatcherService/instruments/FolderWatcherService.cs
+++ FolderWatcherService/instruments/FolderWatcherService.cs
@@ -16,22 +16,29 @@ namespace FolderWatcherBackgroundProgram.instruments
         }
 
 
-       
+
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             string path = _pathConfig.Path;
             Console.WriteLine(path);
             var folderWatcher = new LoggingFolderWatcher(path);
 
+            _logger.LogInformation($"Start watching for ${_pathConfig.Path}");
+
             while (!stoppingToken.IsCancellationRequested)
             {
 
             }
+
+            _logger.LogInformation($"finish watching for {_pathConfig.Path}");
 
             folderWatcher.WriteInfoAboutChangeFolder();
             Task.Delay(2000).Wait();
 
             return Task.CompletedTask;
         }
+
+       
     }
 }
